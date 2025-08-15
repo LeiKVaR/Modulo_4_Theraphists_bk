293d68c1794ed1194b97abfabebbc66bf6ce7784
Alfredo Gonzales Ramirez <ramzvaal@gmail.com>
2025-08-15 09:30:48 -0500

    Corrección de los modelos de la db

--- therapists/models.py
+++ therapists/models.py
@@ -15,9 +15,12 @@ class Therapist(models.Model):
     # Información de contacto
     phone = models.CharField(max_length=15)  # Teléfono
     email = models.EmailField(blank=True, null=True)  # Correo Electrónico
-    location = models.CharField(max_length=255, blank=True, null=True)  # Ubicación (departamento/provincia/distrito)
+    country = models.CharField(max_length=100, blank=True, null=True) # País
+    department = models.CharField(max_length=100, blank=True, null=True) # Departamento
+    province = models.CharField(max_length=100, blank=True, null=True) # Provincia
+    district = models.CharField(max_length=100, blank=True, null=True) # Distrito
     address = models.TextField(blank=True, null=True)  # Dirección de Domicilio
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True) # Foto de Perfil
 
     def __str__(self):
         return f"{self.first_name} {self.last_name_paternal} {self.last_name_maternal or ''}"
